9a1a63a6f553972c4e9ada4e4d2a6538157df540
Russell Hart <russellbot@gmail.com>
2020-06-14 12:34:16 -0400

    connecting database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-tapered-05765',
-      user : 'postgres',
-      password : '',
-      database : 'facerecognition'
+      connectionString : 'process.env.DATABASE_URL',
+      ssl: true,
     }
 });
 
