c2cd6a378e66d239e6291fd640e0c1925bbbfcba
Russell Hart <russellbot@gmail.com>
2020-09-17 13:16:25 -0400

    added session token to register

--- controllers/register.js
+++ controllers/register.js
@@ -1,44 +1,61 @@
-const handleRegister = (req, res, db, bcrypt) => {
-    const { email, name, password } = req.body;
-    if (!email || !name || !password) {
-        return res.status(400).json('incorrect form submission');
-    }
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
+const jwt = require('jsonwebtoken')
+const redisClient = require('./signin').redisClient
+
+const createSessions = (user) => {
+    // create JWT token and return user
+
+    const { id, email } = user
+    const token = signToken(email)
+    return setToken(token, id)
+        .then(() => {
+            return token
         })
+        .catch(console.log)
+}
+
+// Params: key = token, value = id
+const setToken = (token, id) => {
+    return Promise.resolve(redisClient.set(token, id))
+}
+
+const signToken = (email) => {
+    const jwtPayload = { email }
+    return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '2 days' })
+}
+
+const handleRegister = (req, res, db, bcrypt) => {
+    const { email, name, password } = req.body
+    if (!email || !name || !password) {
+        return res.status(400).json('incorrect form submission')
+    }
+    const hash = bcrypt.hashSync(password)
+    db.transaction((trx) => {
+        trx
+            .insert({
+                hash: hash,
+                email: email,
+            }) 
         .into('login')
         .returning('email')
-        .then(loginEmail => {
+        .then((loginEmail) => {
             return trx('users')
             .returning('*')
             .insert({
                 email: loginEmail[0],
                 name: name,
-                joined: new Date()
+                joined: new Date(),
             })
-            .then(user => {
-                res.json(user[0]);
+            .then(async (user) => {
+                const token = await createSessions(user[0])
+                res.json({ user: user[0], token: token });
             })
-        })
-        .then(trx.commit)
+            .then(trx.commit)
+        })        
         .catch(trx.rollback)
     })    
-    .catch(err => res.status(400).json('unable to register'))    
-}
-
-const registerAuthentication = (db, bcrypt,) =>  (req, res) => {
-    const { authorization } = req.headers;
-    return handleRegister(db, bcrypt, req, res)
-            .then(data => {
-                return data.id && data.email ? createSessions(data) : Promise.reject(data)})
-            .then(session => res.json(session))
-            .catch(err => res.status(400).json(err))
+    .catch((err) => res.status(400).json('unable to register'))    
 }
 
 module.exports = {
-    handleRegister: handleRegister,
-    registerAuthentication: registerAuthentication
+    handleRegister: handleRegister
 }
