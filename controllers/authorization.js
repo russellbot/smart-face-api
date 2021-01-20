08907fdcac2f4e3ac916ca87b5bd62dd8843eca4
Russell Hart <russellbot@gmail.com>
2021-01-20 15:51:34 -0500

    register token

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -3,11 +3,11 @@ const redisClient = require('./signin').redisClient;
 const requireAuth = (req, res, next) => {
     const { authorization } = req.headers;
     if(!authorization) {
-        return res.status(401).json('Unauthorized');
+        return res.status(401).json('Unauthorized no authorization token in headers');
     }
     return redisClient.get(authorization, (err, reply) => {
         if (err || !reply) {
-            return res.status(401).json('Unauthorized');
+            return res.status(401).json('Unauthorized from redis');
         }
         console.log('you shall pass')
         return next();
