f07484454a5471f78e94046cc8ec26d30380220e
Russell Hart <russellbot@gmail.com>
2021-01-20 11:37:47 -0500

    changed signin

--- controllers/signin.js
+++ controllers/signin.js
@@ -4,6 +4,24 @@ const redis = require('redis');
 //setup Redis:
 const redisClient = redis.createClient(process.env.REDIS_URL);
 
+const signToken = (email) => {
+    const jwtPayload = { email };
+    return jwt.sign(jwtPayload, 'JWT_SECRET', { expiresIn: '2 days'});
+}
+
+const setToken = (key, value) => Promise.resolve(redisClient.set(key, value));
+
+const createSessions = (user) => {
+    // JWT token, return user data
+    const { email, id } = user;
+    const token = signToken(email);
+    return setToken(token, id)
+        .then(() => { 
+            return { success: 'true', userId: id, token }
+        })
+        .catch(console.log)
+}
+
 const handleSignin = (db, bcrypt, req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
@@ -33,26 +51,6 @@ const getAuthTokenId = (req, res) => {
         }
         return res.json({id: reply})
     })
-}
-
-const signToken = (email) => {
-    const jwtPayload = { email };
-    return jwt.sign(jwtPayload, 'JWT_SECRET', { expiresIn: '2 days'});
-}
-
-const setToken = (key, value) => {
-    return Promise.resolve(redisClient.set(key, value))
-}
-
-const createSessions = (user) => {
-    // JWT token, return user data
-    const { email, id } = user;
-    const token = signToken(email);
-    return setToken(token, id)
-        .then(() => { 
-            return { success: 'true', userId: id, token }
-        })
-        .catch(console.log)
 }
 
 const signinAuthentication = (db, bcrypt,) =>  (req, res) => {
